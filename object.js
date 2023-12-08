f3c1d170fb3df58a3e856a9130e470df76fdbd27
Anurag Samarth <asamarth017@gmail.com>
2023-12-08 20:03:16 +0530

    30-days-of-JavaScript

--- object.js
+++ object.js
@@ -2,7 +2,7 @@
                                                                         
 // Exercises: Level 1
 
-// //Q1.  Create an empty object called dog.     // Q2.. Print the the dog object on the console. 
+//Q1.  Create an empty object called dog.     // Q2.. Print the the dog object on the console. 
 const dog = {};
 console.log(dog);
 
@@ -261,7 +261,7 @@ const countries = [
     console.log('Language:',countryObject.languages);
  }
                                                         // OR 
-  const country = countries[Object.keys(countries)[0]];
+  const country = countries[Object.keys(countries)[0]];              // // This line of code is get the value of the array object if there is not key. 
    
   console.log("Name: ", country.name)
   console.log("Capital: ", country.capital)
@@ -301,13 +301,169 @@ const personAccount = {
   console.log(expensesTotal);
   }
 }
-
 console.log(personAccount.totalIncome())
 console.log(personAccount.totalExpenses())
 
 
 
+// //   **** Questions:2, 3 and 4 are based on the following two arrays:users and products () 
+let users = [
+  {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt:'08/01/2020 9:00 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt:'08/01/2020 9:30 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt:'08/01/2020 9:45 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt:'08/01/2020 9:50 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt:'08/01/2020 10:00 AM',
+      isLoggedIn: false
+  }
+  ];
+
+  // // Q14.  Imagine you are getting the above users collection from a MongoDB database. 
+  // // Q14[a]. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
+  const createCollection = (name, email, pass) => {
+    for (let i = 0; i < users.length; i++) {
+      let signUp = users[Object.keys(users)[i]];
+      if (
+        signUp.username === name &&
+        signUp.email === email &&
+        signUp.password === pass
+      ) {
+        return (`username is already exeist`);
+      } }
+      // // Create a user ID
+        let randomNum = `1234567890abcdefghijk`;
+        let userIds = [];
+        let createDate = new Date();
+        for (let i = 0; i < 6; i++) {
+          userIds.push(randomNum[Math.floor(Math.random() * randomNum.length)]);
+        }
+        
+        // // Create Current Date and Time
+        let currentDate = createDate.toLocaleDateString();
+        let currentTime = createDate.toLocaleTimeString();
+
+        
+        // // Create Object and push into user Array 
+        const newPerson = {
+          _id: userIds.join(""),
+          username: name,
+          email: email,
+          password: pass,
+          createdAt: `${currentDate} ${currentTime}`,
+          isLoggedIn: false,
+        };
+        users.push(newPerson);
+        return users;
+      }
+  console.log(createCollection("Anu", "Anu123@gmai.com", "jhfkas"));
 
 
+
+  // // Q14[b]. Create a function called signIn which allows user to sign in to the application. 
+  let signIN = (mail, pswd) => {
+    for(const user of users) {
+       if(user.email === mail && user.password === pswd) {
+          user.isLoggedIn = true;
+          return 'You have been signed in!!';
+       }  
+    }
+    return 'Incorrect Email or password';
+ }
+ console.log(signIN('thomsdjfdsbs.com', '1sjhks3'))
+console.log(users);
+
+
+// // Q15. The products array has three elements and each of them has six properties.
+let products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+]
+
+// // Q15[a]  Create a function called rateProduct which rates the product. 
+const rateProduct = (name,rating)=>{
+     for(const product of products){
+       if(product.name === name){
+        products.push(product.ratings = rating);
+        return products;
+       } else{
+        return `Product name is does not match`;
+       }
+     }
+}
+console.log(rateProduct('Laptop',4))
+
+
+// // Q15[b] Create a function called averageRating which calculate the average rating of a product.
+const averageRating = (name)=>{
+      let total = 0;
+      let pdt;
+      for(const product of products){
+        if(product.name === name){
+           pdt = product;
+        }
+      }       
+     for(const usr of pdt.ratings){
+        total += usr.rate;
+     }
+     return total/pdt.ratings.length;
+}
+console.log(averageRating('mobile phone'));
 
 
